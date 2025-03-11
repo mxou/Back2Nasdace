c429ff0d09cff4727f24d9e2a4b2f9e2db212954
mxou <maximilien.dlrm@gmail.com>
2025-03-11 14:27:00 +0100

    animation du ship

--- src/components3D/Ship.jsx
+++ src/components3D/Ship.jsx
@@ -6,9 +6,19 @@ import * as THREE from "three";
 
 export default function Ship({ position, scale = [5, 5, 5] }) {
   const { scene } = useGLTF("./src/assets/modeles/ship.glb");
+  const shipRef = useRef();
+  const [isFlying, setIsFlying] = useState(false);
+
+  useFrame(() => {
+    if (isFlying && shipRef.current) {
+      shipRef.current.position.y += 0.05; // Monte doucement
+      shipRef.current.rotation.y += 0.01; // Tourne légèrement
+    }
+  });
+
   return (
-    <RigidBody colliders="hull" gravityScale={1} restitution={0.5}>
-      <primitive object={scene} position={position} scale={scale} />;
+    <RigidBody colliders="hull" gravityScale={1} restitution={0.5} type="dynamic" mass={5} friction={1}>
+      <primitive object={scene} position={position} scale={scale} ref={shipRef} onClick={() => setIsFlying(!isFlying)} />;
     </RigidBody>
   );
 }
